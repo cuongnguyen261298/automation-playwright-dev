1f93f4d46f29e9f462d8a219b6ba403b3cac7390
Cuong Nguyen <cnp@localizedirect.com>
2025-08-20 18:03:27 +0700

    fix: storage account

--- src/tests/Saucedemo/InventoryTest.test.ts
+++ src/tests/Saucedemo/InventoryTest.test.ts
@@ -1,29 +1,37 @@
 import { test } from "../../core/baseTest";
 import { UserCredential } from "../../core/sharedUserData";
 
-test.describe("Add to cart suite>", () => {
-  // arrange
-  const productList = [
-    "Sauce Labs Backpack",
-    "Sauce Labs Bike Light",
-    "Sauce Labs Fleece Jacket",
-    "Sauce Labs Bolt T-Shirt",
-  ];
+// arrange
+const productList = [
+  "Sauce Labs Backpack",
+  "Sauce Labs Bike Light",
+  "Sauce Labs Fleece Jacket",
+  "Sauce Labs Bolt T-Shirt",
+];
 
-  const userListHaveTest = [
-    UserCredential.STANDARD_USER,
-    UserCredential.LOCKED_OUT_USER,
-    UserCredential.PROBLEM_USER,
-    UserCredential.ERROR_USER,
-    UserCredential.VISUAL_USER,
-  ];
+const userListHaveTest = [
+  UserCredential.STANDARD_USER,
+  // UserCredential.LOCKED_OUT_USER,
+  UserCredential.PROBLEM_USER,
+  UserCredential.ERROR_USER,
+  UserCredential.VISUAL_USER,
+];
 
-  test.beforeEach(async ({ signInPage }) => {
-    await signInPage.goTo();
-    await signInPage.signInWith(userListHaveTest[0]);
+for (const user of userListHaveTest) {
+  test.describe("Add to cart suite >", () => {
+    test.use({
+      storageState: user,
+    });
+    // arrange
+    test.beforeEach(async ({ inventoryPage }) => {
+      await inventoryPage.goTo();
+    });
+    test.afterEach(async ({}) => {});
+
+    test(`should add product list with storage ${user}`, async ({
+      inventoryPage,
+    }) => {
+      await inventoryPage.addtoCartWithProductName(productList);
+    });
   });
-
-  test(`should add product list`, async ({ inventoryPage }) => {
-    await inventoryPage.addtoCartWithProductName(productList);
-  });
-});
+}
--- src/tests/Saucedemo/SignInTest.test.ts
+++ src/tests/Saucedemo/SignInTest.test.ts
@@ -3,24 +3,43 @@ import { UserCredential } from "../../core/sharedUserData";
 
 const userListHaveTest = [
   UserCredential.STANDARD_USER,
-  UserCredential.LOCKED_OUT_USER,
+  // UserCredential.LOCKED_OUT_USER,
   UserCredential.PROBLEM_USER,
   UserCredential.ERROR_USER,
   UserCredential.VISUAL_USER,
 ];
+
 for (const user of userListHaveTest) {
-  test.describe("Login suite >", () => {
+  test.describe("Login suite - approach 1 >", () => {
     test.use({
       storageState: user,
     });
     // arrange
-    test.beforeEach(async ({ signInPage }) => {
-      await signInPage.goTo();
-      await signInPage.signInWith(user);
+    test.beforeEach(async ({ inventoryPage }) => {
+      await inventoryPage.goTo();
     });
     test.afterEach(async ({}) => {});
-
-    test(`should login successful with ${user}`, async ({ signInPage }) => {
-    });
+    test(`should login successful with storage ${user}`, async ({}) => {});
   });
 }
+
+[
+  { account: UserCredential.STANDARD_USER },
+  // { account: UserCredential.LOCKED_OUT_USER },
+  { account: UserCredential.PROBLEM_USER },
+  { account: UserCredential.ERROR_USER },
+  { account: UserCredential.VISUAL_USER },
+].forEach(({ account }) => {
+  test.describe("Login suite - approach 2 >", () => {
+    test.use({
+      storageState: account,
+    });
+
+    test.beforeEach(async ({ inventoryPage }) => {
+      await inventoryPage.goTo();
+    });
+
+    test.afterEach(async ({}) => {});
+    test(`should login successful with storage ${account}`, async ({}) => {});
+  });
+});
